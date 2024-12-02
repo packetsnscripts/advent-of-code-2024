bcdb5d29b9e898e7af13c53959e0772243c9ffd0
Siddharth Shettigar <ss7153@nyu.edu>
2024-12-01 16:25:41 -0800

    rest of day1 + some repo setup

--- src/day01/solution_1.py
+++ src/day01/solution_1.py
@@ -1,12 +1,47 @@
+def sum_of_diffs(list1: list, list2: list) -> int:
+    """
+    Takes 2 lists of ints and returns the sum of the difference between ith element.
+    """
+    if len(list1) != len(list2):
+        raise ValueError("Lists must be of equal length")
 
-list1 = []
-list2 = []
-with open('src/day01/input_1.txt', "r") as file:
-    for line in file:
-        print(line)
-        num1, num2 = file.readline().split("   ")
-        print(f"{num1} {num2}")
-        list1.append(num1)
-        list2.append(num2)
-    
-# print(list1)
+    list1.sort()
+    list2.sort()
+    return sum(abs(a - b) for a, b in zip(list1, list2))
+
+
+def occurences(list) -> dict:
+    num_of_occurences = {}
+    for num in list:
+        if num in num_of_occurences:
+            num_of_occurences[num] += 1
+        else:
+            num_of_occurences[num] = 1
+
+    return num_of_occurences
+
+
+def similarity_score(list1: list, list2: list):
+    """
+    similarity score by adding up each number in the left list after multiplying
+    it by the number of times that number appears in the right list
+    """
+    list2_occurences = occurences(list2)
+    return sum(num * list2_occurences.get(num, 0) for num in list1)
+
+
+def main():
+    list1 = []
+    list2 = []
+    with open("src/day01/input_1.txt", "r") as file:
+        for line in file:
+            num1, num2 = line.split("   ")
+            list1.append(int(num1))
+            list2.append(int(num2))
+
+    print(f"Sum of differences in input = {sum_of_diffs(list1,list2)}")
+    print(f"Similarity score of lists = {similarity_score(list1,list2)}")
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,2 +1,25 @@
-from src.day01 import day01
+import pytest
 
+from src.day01.solution_1 import occurences, similarity_score, sum_of_diffs
+
+
+@pytest.fixture
+def test_lists():
+    list1 = [3, 4, 2, 1, 3, 3]
+    list2 = [4, 3, 5, 3, 9, 3]
+    return list1, list2
+
+
+def test_sum_of_diffs(test_lists):
+    list1, list2 = test_lists
+    assert sum_of_diffs(list1, list2) == 11
+
+
+def test_occurences(test_lists):
+    _, list2 = test_lists
+    assert occurences(list2) == {3: 3, 4: 1, 5: 1, 9: 1}
+
+
+def test_similarity_score(test_lists):
+    list1, list2 = test_lists
+    assert similarity_score(list1, list2) == 31
